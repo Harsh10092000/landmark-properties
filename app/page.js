4d1d424fe5cef9179199c6c5b8cd33193c51635d
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-11 12:43:26 +0530

    commit

---
+++ app/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./style.scss"
+const page = () => {
+  return (
+    <>
+      <div className="container text-center main">
+  <div className="row column-1">
+    <div className="col">
+      Column 1
+    </div>
+    <div className="col">
+      Column
+    </div>
+    <div className="col">
+      Column
+    </div>
+  </div>
+  <button className='btn btn-danger'>hello</button>
+  <button type="button" class="btn btn-primary">Primary</button>
+</div>
+    </>
+  )
+}
+
+export default page
