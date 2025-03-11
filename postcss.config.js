d92aa2057c78810d876e851451271ae140754965
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-11 12:49:26 +0530

    commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const purgecss = require('postcss-purgecss')({
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  });
+  
+  module.exports = {
+    plugins: [
+      'postcss-preset-env',
+      ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ],
+  };
