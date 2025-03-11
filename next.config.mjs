7d1d1e16c66b49ee30da762c533a04c9ce882adb
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-11 13:21:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        cssChunking: true, // default
+      },
+};
 
 export default nextConfig;
