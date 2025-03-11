a4918a831564887ecb212ee75bbaa1b17697d028
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-11 13:13:01 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,20 @@
-module.exports = {
+const purgecss = require('postcss-purgecss')({
+    content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'], // Adjust paths as needed
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  });
+  
+  module.exports = {
     plugins: {
-        "postcss-flexbugs-fixes": {},
-        "postcss-preset-env": {
-            "autoprefixer": {
-                "flexbox": "no-2009"
-            },
-            "stage": 3,
-            "features": {
-                "custom-properties": false
-            }
+      "postcss-flexbugs-fixes": {},
+      "postcss-preset-env": {
+        autoprefixer: {
+          flexbox: "no-2009",
         },
+        stage: 3,
+        features: {
+          "custom-properties": true, // Enable custom properties (recommended unless you have a specific reason to disable)
+        },
+      },
+      ...(process.env.NODE_ENV === 'production' ? [purgecss] : []), // Apply PurgeCSS only in production
     },
-  }
+  };
