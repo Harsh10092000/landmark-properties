7689678ccb558ce52462135c5d2ee1dae5747ba8
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-11 15:53:17 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         cssChunking: true, // default
+//       },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        cssChunking: true, // default
-      },
-};
-
-export default nextConfig;
+      cssChunking: true, // Optional, as it's the default in Next.js 15
+    },
+  };
+  
+  import withBundleAnalyzer from '@next/bundle-analyzer';
+  
+  const bundleAnalyzerConfig = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+  });
+  
+  export default bundleAnalyzerConfig(nextConfig);
--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,23 @@
-const purgecss = require('postcss-purgecss')({
-    content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'], // Adjust paths as needed
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  });
-  
-  module.exports = {
-    plugins: {
-      "postcss-flexbugs-fixes": {},
-      "postcss-preset-env": {
-        autoprefixer: {
-          flexbox: "no-2009",
-        },
-        stage: 3,
-        features: {
-          "custom-properties": true, // Enable custom properties (recommended unless you have a specific reason to disable)
-        },
-      },
-      ...(process.env.NODE_ENV === 'production' ? [purgecss] : []), // Apply PurgeCSS only in production
-    },
-  };
+const isProduction = true; // Hardcode for testing; replace with environment logic if needed
+module.exports = {
+    plugins:
+    isProduction === 'production'
+        ? [
+            'postcss-flexbugs-fixes',
+            [
+              'postcss-preset-env',
+              {
+                autoprefixer: {
+                  flexbox: 'no-2009',
+                },
+                stage: 3,
+                features: {
+                  'custom-properties': false,
+                },
+              },
+            ],
+          ]
+        : [
+            // No transformations in development
+          ],
+  }
