0a8357a8399893ad23404ab688f7388e834d46d5
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-12 10:14:39 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 
-import "./globals.css";
-import  "bootstrap/dist/css/bootstrap.min.css"
+//import "./globals.css";
+//import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap/dist/css/bootstrap.min.css"
 //import "../styles/custom-bootstrap.scss";
-//import "@/app/styles/custom-bootstrap.scss"
+import "@/app/styles/custom-bootstrap.scss"
 import AddBootstrap from "./AddBootstrap";
 
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // experimental: {
+    //     cssChunking: true, // default
+    //   },
+};
+
+export default nextConfig;
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //     experimental: {
-//         cssChunking: true, // default
-//       },
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      cssChunking: true, // Optional, as it's the default in Next.js 15
-    },
-  };
+//       cssChunking: true, // Optional, as it's the default in Next.js 15
+//     },
+    
+//   };
   
-  import withBundleAnalyzer from '@next/bundle-analyzer';
+//   import withBundleAnalyzer from '@next/bundle-analyzer';
   
-  const bundleAnalyzerConfig = withBundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
-  });
+//   const bundleAnalyzerConfig = withBundleAnalyzer({
+//     enabled: process.env.ANALYZE === 'true',
+//   });
   
-  export default bundleAnalyzerConfig(nextConfig);
+//   export default bundleAnalyzerConfig(nextConfig);
